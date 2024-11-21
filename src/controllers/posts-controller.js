329ac3294c542bc2f7da93ef1fc5c8d23b02dca9
marilia.garcia <marilia.garcia@logiquesistemas.com.br>
2024-11-21 15:21:47 +0100

    image upload

--- src/controllers/posts-controller.js
+++ src/controllers/posts-controller.js
@@ -1,4 +1,5 @@
 import { getAllPosts, createPost } from "../models/posts-model.js";
+import fs from "fs";
 
 export async function listPosts(req, res) {
  const posts = await getAllPosts();
@@ -12,6 +13,23 @@ export async function sendPost(req, res) {
   res.status(200).json(createdPost);
  } catch (err) {
   console.error(err);
-  res.status(500);
+  res.status(500).json({ Erro: "Falha na requisição" });
+ }
+}
+
+export async function uploadImage(req, res) {
+ const newPost = {
+  descricao: "",
+  imgUrl: req.file.originalname,
+  alt: "",
+ };
+ try {
+  const createdPost = await createPost(newPost);
+  const updatedImage = `uploads/${createdPost.insertedId}.png`;
+  fs.renameSync(req.file.path, updatedImage);
+  res.status(200).json(createdPost);
+ } catch (err) {
+  console.error(err.message);
+  res.status(500).json({ Erro: "Falha na requisição" });
  }
 }
--- src/routes/posts-routes.js
+++ src/routes/posts-routes.js
@@ -1,12 +1,20 @@
+import multer from "multer";
 import express from "express";
 
-import { sendPost, listPosts } from "../controllers/posts-controller.js";
+import {
+ sendPost,
+ listPosts,
+ uploadImage,
+} from "../controllers/posts-controller.js";
+
+const upload = multer({ dest: "./uploads" });
 
 const routes = (app) => {
  app.use(express.json());
 
  app.get("/posts", listPosts);
  app.post("/posts", sendPost);
+ app.post("/upload", upload.single("image"), uploadImage);
 };
 
 export default routes;
